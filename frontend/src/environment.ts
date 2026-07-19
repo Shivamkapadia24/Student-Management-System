5b209c8328434a5cd527390aa34546ce1a90ac0a
Shivamkapadia24 <shivamkapadia123@gmail.com>
2026-07-19 16:58:00 +0530

    Config: Update backend Render API endpoint subdomain to ztcy

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   apiUrl: window.location.hostname === 'localhost'
     ? 'http://localhost:5000'
-    : 'https://student-management-system-backend-z8qy.onrender.com'
+    : 'https://student-management-system-backend-ztcy.onrender.com'
 };
